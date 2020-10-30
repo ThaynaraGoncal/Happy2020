caaa0938b9316e3c2d602b58227fe76df4ca2ab7
thaynaragoncal <thaynarafaria21@gmail.com>
2020-10-30 15:53:08 -0300

    Feito primeira rota + createdados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import './database/connection';
+import './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello Thaynara' });
-})
+app.use(routes);
 
 app.listen(3333);
